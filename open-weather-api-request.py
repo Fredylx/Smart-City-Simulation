37fb684712c4991f6d7880648c3de5d4c8bc4a78
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-08-24 11:32:09 -0700

    Create open-weather-api-request.py

---
+++ open-weather-api-request.py
@@ -0,0 +1,13 @@
+from http import *
+from realhttp import *
+from time import *
+import json
+
+
+# open weather url for Tampa
+API_KEY = "" #add API key from open weather
+LAT = "27.7634"
+LON = "82.5437"
+CITY = "Tampa"
+
+URL = "https://api.openweather"
